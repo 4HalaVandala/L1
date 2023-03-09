d3874b0d0e6a8fb6ba75e6d14fd8c979667b0fd9
4halavandala <1terrormachine1@gmail.com>
2023-03-09 17:15:20 +0300

    3 added

--- Tasks/3.go
+++ Tasks/3.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	nums := []int{2, 4, 6, 8, 10}
+	results := make(chan int, len(nums))
+
+	// создаем пул горутин
+	pool := sync.Pool{
+		New: func() interface{} {
+			return make([]byte, 4)
+		},
+	}
+
+	for _, n := range nums {
+		wg.Add(1)
+		go func(x int) {
+			defer wg.Done()
+			buf := pool.Get().([]byte)
+			defer pool.Put(buf)
+			res := x * x
+			str := fmt.Sprintf("%d\n", res)
+			copy(buf, str)
+			results <- res
+		}(n)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	sum := 0
+	for res := range results {
+		sum += res
+	}
+
+	fmt.Println(sum)
+}
